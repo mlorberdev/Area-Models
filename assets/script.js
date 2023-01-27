926987c95b3926ea1e9ce9ff9325f502649760f5
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-01-27 01:38:22 -0500

    chg arr to static html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 !(function () {
   const rn = (a, b) => { return Math.floor(Math.random() * (b - a)) + a; }
-  const array = document.getElementById("array");
   const mm = document.getElementById("n1");
   const nn = document.getElementById("n2");
   let m, n, A = [], r = 3, c = 2;
@@ -11,33 +10,21 @@
     // WRITE TO DOM
     mm.innerHTML = m;
     nn.innerHTML = n;
-    // DECOMPOSITION
+    document.getElementById("n3").innerHTML = (m * n).toLocaleString();
+    // DECOMPOSITION & FILL OUTER
     let rr = m.toString().split("");
     let cc = n.toString().split("");
     rr = [rr[0] * 100, rr[1] * 10, rr[2] * 1];
-    cc = [cc[0] * 10, cc[1] * 10];
-    // CREATE & FILL ARRAY
-    let frag = new DocumentFragment();
-    for (let i = 0; i < rr.length; i++) {
-      let R = document.createElement("div");
-      R.classList.add("flex","row","center");
-      for (let j = 0; j < cc.length; j++) {
-        let C = document.createElement("div");
-        C.classList.add("flex","center","arr");
-        let I = document.createElement("input");
-        I.type = "tel";
-        I.maxLength = 5;
-        I.size = 5;
-        I.addEventListener("keydown", kick);
-        C.appendChild(I);
-        R.appendChild(C);
-      }
-      frag.appendChild(R);
-    }
-    array.appendChild(frag);
+    cc = [cc[0] * 10, cc[1] * 1];
+    // FILL OUTER
+    for (let i = 0; i < rr.length; i++) document.getElementById(`r${i}`).innerHTML = rr[i];
+    for (let i = 0; i < cc.length; i++) document.getElementById(`c${i}`).innerHTML = cc[i];
   }
   init();
-  function kick(e) {
-    if (!e.key.match(/[0-9]/) && e.which !== 8 && e.which !== 9) e.preventDefault();
-  }
+
+  document.getElementById("new").addEventListener("click", init);
+  document.querySelectorAll("input").forEach(input => input.addEventListener("keydown", function (e) {
+    if (!e.key.match(/[0-9]/) && e.key !== 'Backspace' && e.key !== 'Enter') e.preventDefault();
+  }));
+  
 })();
