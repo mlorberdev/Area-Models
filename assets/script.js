f47be32f3bd0ae8134b5711789876a49a3779490
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-01-27 12:49:15 -0500

    prototype v0.1.0

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 !(function () {
   const rn = (a, b) => { return Math.floor(Math.random() * (b - a)) + a; }
-  const steps = document.getElementById("steps");
   const mm = document.getElementById("n1");
   const nn = document.getElementById("n2");
-  let m, n, rr, cc, N, arr = [], r = 3, c = 2;
+  let m, n, rr, cc, N = 0, arr = [], r = 3, c = 2;
+  const sums = document.getElementById("sums");
   function init() {
-    N = 0;
-    if (!steps.classList.contains("hide")) steps.classList.toggle("hide");
-    document.querySelectorAll("input").forEach(input => { input.maxLength = 4; input.size = 4; });
+    if (!sums.classList.contains("hide")) sums.classList.add("hide");
+    document.querySelectorAll(".review").forEach(rev => { if (!rev.classList.contains("hide")) rev.classList.add("hide")});
+    document.getElementById("step").innerHTML = "The first step is to MULTIPLY the ROW and the COLUMN";
     m = rn(100, 400);
     n = rn(11, 99);
     mm.innerHTML = m;
@@ -20,24 +20,46 @@
     document.querySelectorAll("input").forEach(input => input.value = "");
     for (let i = 0; i < rr.length; i++) document.getElementById(`r${i}`).innerHTML = rr[i];
     for (let i = 0; i < cc.length; i++) document.getElementById(`c${i}`).innerHTML = cc[i];
+    for (let i = 0; i < rr.length; i++) for (let j = 0; j < cc.length; j++)
+      document.getElementById(`arr${i}${j}`).name = `${rr[i] * cc[j]}`;
   }
   init();
 
+  // NEW
   document.getElementById("new").addEventListener("click", init);
-  document.querySelectorAll("input").forEach(input => input.addEventListener("keydown", function (e) {
+
+  // INPUT EVENT
+  document.querySelectorAll("input").forEach(input => input.addEventListener("keyup", function (e) {
     if (!e.key.match(/[0-9]/) && e.key !== 'Backspace' && e.key !== 'Tab') e.preventDefault();
+    this.value !== this.name ? this.style.color = "white" : this.style.color = "#65e60f";
   }));
+
+  // SOLVE
   document.getElementById("solve").addEventListener("click", function () {
-    document.querySelectorAll("input").forEach(input => { input.maxLength = 30; input.size = 14});
-    for (let i = 0; i < rr.length; i++) for (let j = 0; j < cc.length; j++)
-      document.getElementById(`arr${i}${j}`).value = `${rr[i]} × ${cc[j]} = ${rr[i] * cc[j]}`;
-    steps.classList.toggle("hide");
+    for (let i = 0; i < rr.length; i++) for (let j = 0; j < cc.length; j++) {
+      let cell = document.getElementById(`arr${i}${j}`);
+      cell.value = cell.name;
+      cell.style.color = "#65e60f";
+    }
   });
+
+  // NEXT STEP
   document.getElementById("next").addEventListener("click", function () {
-    switch (N) {
-      case 0: break;
-      case 1: break;
-      case 2: break;
+    if (N === 0) {
+      document.getElementById("step").innerHTML = "<span style='color:aqua'>The second step is to ADD the PRODUCTS, then you're done 👏</span>";
+      sums.classList.remove("hide");
+      let ls = 0, rs = 0;
+      for (let i = 0; i < 3; i++) {
+        ls += parseInt(document.getElementById(`arr${i}0`).name);
+        rs += parseInt(document.getElementById(`arr${i}1`).name);
+      }
+      document.getElementById("arr30").value = ls;
+      document.getElementById("arr31").value = rs;
+      N++;
+    } else {
+      document.getElementById("arr40").value = `${m} × ${cc[0]} = ${m * cc[0]}`;
+      document.getElementById("arr41").value = `${m} × ${cc[1]} = ${m * cc[1]}`;
+      document.querySelectorAll(".review").forEach(rev => rev.classList.remove("hide"));
     }
   });
 
